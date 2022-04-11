860b57b4946f5c5c542c834a52c943b22482244e
chris241 <chrisleon325@gmail.com>
2022-04-11 07:52:00 +0300

    code

--- Test.kata2-fizzbuzz2/UnitTest1.cs
+++ Test.kata2-fizzbuzz2/UnitTest1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using kata2_fizzbuzz2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace Test.kata2_fizzbuzz2
@@ -7,8 +8,46 @@ namespace Test.kata2_fizzbuzz2
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Generate_Val1_Return1()
         {
+            var fizz = FizzBuzz.Generate("1");
+            Assert.AreEqual(fizz, 1);
+        }
+        [TestMethod]
+        public void Generate_Val2_Return2()
+        {
+            var fizz = FizzBuzz.Generate("2");
+            Assert.AreEqual(fizz, 2);
+        }
+        [TestMethod]
+        public void Generate_Val3_ReturnFizz()
+        {
+            var fizz = FizzBuzz.Generate("3");
+            Assert.AreEqual(fizz, "Fizz");
+        }
+        [TestMethod]
+        public void Generate_Val4_Return4()
+        {
+            var fizz = FizzBuzz.Generate("4");
+            Assert.AreEqual(fizz, 4);
+        }
+        [TestMethod]
+        public void Generate_Val5_ReturnBuzz()
+        {
+            var fizz = FizzBuzz.Generate("5");
+            Assert.AreEqual(fizz, "Buzz");
+        }
+        [TestMethod]
+        public void Generate_Val6_ReturnFizz()
+        {
+            var fizz = FizzBuzz.Generate("6");
+            Assert.AreEqual(fizz, "Fizz");
+        }
+        [TestMethod]
+        public void Generate_Val15_ReturnFizzBuzz()
+        {
+            var fizz = FizzBuzz.Generate("15");
+            Assert.AreEqual(fizz, "FizzBuzz");
         }
     }
 }
---
+++ kata2-fizzbuzz2/FizzBuzz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata2_fizzbuzz2
+{
+    public static class FizzBuzz
+    {
+        public static object Generate(string number)
+        {
+            if (IsDivisibleBy3(number, 15)) return "FizzBuzz";
+            if (IsDivisibleBy3(number,3)) return "Fizz";
+            if (IsDivisibleBy3(number, 5)) return "Buzz";
+            return int.Parse(number);
+
+        }
+
+        private static bool IsDivisibleBy3(string number, long div)
+        {
+            return int.Parse(number) % div == 0;
+        }
+    }
+}
